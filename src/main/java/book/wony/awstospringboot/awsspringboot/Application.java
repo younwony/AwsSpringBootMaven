8d6c1cdaf69ca10f1293b2030b449714d88c9178
younwony <wony9324@naver.com>
2021-04-09 22:52:52 +0900

    [추가] 게시판 게시판 기능 전체 완료

--- src/main/java/book/wony/awstospringboot/awsspringboot/Application.java
+++ src/main/java/book/wony/awstospringboot/awsspringboot/Application.java
@@ -2,10 +2,7 @@ package book.wony.awstospringboot.awsspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
